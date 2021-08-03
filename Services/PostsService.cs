0a81dc9661243c7ba6bf9823674a8b4511613224
Kris Jones <kristop.jones@gmail.com>
2021-08-03 15:00:56 -0600

    gets and posts for posts

---
+++ Services/PostsService.cs
@@ -0,0 +1,23 @@
+using System;
+using sqwuakServer.Models;
+
+namespace sqwuakServer.Services
+{
+    public class PostsService
+    {
+        internal object GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Post Create(Post newPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
