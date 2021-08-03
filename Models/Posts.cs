eb8c497e7005bbcf812f6f2e0a0f884bb0cacfe6
Kris Jones <kristop.jones@gmail.com>
2021-08-03 14:05:52 -0600

    Posts model

--- Models/Posts.cs
+++ Models/Posts.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sqwuakServer.Models
 {
     public class Posts
     {
         public int Id { get; set; }
         public string CreatorId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public int Views { get; set; }
+        public int Shares { get; set; }
+        public int Saves { get; set; }
+        public Account Creator { get; set; }
     }
 }
