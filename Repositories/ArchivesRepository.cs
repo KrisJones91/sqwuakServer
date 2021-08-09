6573a89c41f537e4cd70df96a1e7933768adae0f
Kris Jones <kristop.jones@gmail.com>
2021-08-09 14:00:12 -0600

    Archives Service from Account Controller

--- Repositories/ArchivesRepository.cs
+++ Repositories/ArchivesRepository.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections.Generic;
+using sqwuakServer.Models;
+
 namespace sqwuakServer.Repositories
 {
     public class ArchivesRepository
     {
-        
+        //From ACCOUNT Controller
+        internal IEnumerable<Archive> GetArchivesByOwnerId(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/ArchivesService.cs
+++ Services/ArchivesService.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using sqwuakServer.Models;
+using sqwuakServer.Repositories;
 
 namespace sqwuakServer.Services
 {
     public class ArchivesService
     {
+        private readonly ArchivesRepository _arepo;
+        public ArchivesService(ArchivesRepository arepo)
+        {
+            _arepo = arepo;
+        }
         internal IEnumerable<Archive> GetArchivesByAccountId(string id)
         {
-            throw new NotImplementedException();
+            return _arepo.GetArchivesByOwnerId(id);
         }
     }
 }
