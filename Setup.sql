ed8e8c48d173f93c761332796b3fc2e9f25f1acb
Kris Jones <kristop.jones@gmail.com>
2021-09-30 16:45:04 -0600

    updated sql table

--- Setup.sql
+++ Setup.sql
@@ -18,8 +18,8 @@
 --   saves int DEFAULT 0,
 --   creatorId varchar(255) comment 'FK: Account Id',
 --   FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
--- ) default charset utf8 comment '';
--- CREATE TABLE vaults(
+-- -- ) default charset utf8 comment '';
+-- CREATE TABLE archives(
 --   id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
 --   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
 --   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
@@ -48,4 +48,4 @@
 --   FOREIGN KEY (creatorId) REFERENCES accounts(id),
 --   FOREIGN KEY (postId) REFERENCES posts(id) ON DELETE CASCADE
 -- ) default charset utf8 comment '';
--- DROP TABLE accounts;
+-- DROP TABLE vaults;
